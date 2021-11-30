e03db451d2aab706cf23245ae4e802a921963c10
sanaul01 <mdsanaulislam154@gmail.com>
2021-11-30 17:10:10 +0600

    contact site added

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.scss"
 
 export default function Contact(){
+
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
-            <h2>Contact</h2>
+            <div className="left">
+                <img src="asset/Email.png" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll Replay Soon : )</span>}
+                </form>
+            </div>
         </div>
     )
 }
