2ef5fdac54610146fac87dfa982e8d536e35776e
sanaul01 <mdsanaulislam154@gmail.com>
2021-11-30 16:18:46 +0600

    testimonial added

--- src/Components/Home/Testimonials/Testimonials.jsx
+++ src/Components/Home/Testimonials/Testimonials.jsx
@@ -3,9 +3,55 @@ import "./Testimonials.scss"
 
 
 export default function Testimonials(){
+
+    const deta =[
+        {
+            id: 1,
+            name: "Rahul",
+            title: "Sinior Developer",
+            icon: "asset/logo7.png",
+            img: "https://images.pexels.com/photos/1280162/pexels-photo-1280162.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, itaque.",
+        },
+        {
+            id: 2,
+            name: "Joker",
+            title: "Sinior React Developer",
+            icon: "asset/logo7.png",
+            img: "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, itaque.",
+            featured: true,
+        },
+        {
+            id: 3,
+            name: "Tekka",
+            title: "Sinior Full-Stack Developer",
+            icon: "asset/logo7.png",
+            img: "https://media.gettyimages.com/photos/success-picture-id912928582?s=612x612",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, itaque.",
+        },
+    ]
     return (
         <div className="testimonials" id="testimonials">
-            <h2>Testimonials</h2>
+            <h1>Testimonials</h1>
+            <div className="container">
+                {deta.map((d)=>(
+                <div className={d.featured ? "card featured" : "card"}>
+                    <div className="top">
+                        <img src="asset/right-arrow.png" className="left" alt="" />
+                        <img className="user" src={d.img} alt="" />
+                        <img className="right" src={d.icon} alt="" />
+                    </div>
+                    <div className="center">
+                        {d.desc}
+                    </div>
+                    <div className="bottom">
+                        <h3>{d.name}</h3>
+                        <h4>{d.title}</h4>
+                    </div>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/Components/Home/Works/Works.jsx
+++ src/Components/Home/Works/Works.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Works.scss"
 
 export default function Works(){
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    const deta = [
+        {
+            id: "1",
+            icon: "asset/logo4.png",
+            title: "Web App",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            img: "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        },
+        {
+            id: "2",
+            icon: "asset/logo6.png",
+            title: "React App",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            img: "https://crowdbotics.ghost.io/content/images/2021/05/React-Native-Img.png"
+        },
+        {
+            id: "3",
+            icon: "asset/logo7.png",
+            title: "JavaSctipt",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            img: "https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&w=1000&q=80"
+        },
+    ];
+
+    const handleClick= (way)=>{
+        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) : 
+        setCurrentSlide(currentSlide < deta.length -1 ? currentSlide + 1 : 0)
+    }
+
     return (
         <div className="works" id="works">
-            <div className="slider">
+            <div className="slider" style={{transform:`translateX(-${currentSlide *100}vw)`}}>
+                {deta.map((d)=>(
                 <div className="container">
                     <div className="item">
                         <div className="left">
                             <div className="leftContainer">
                                 <div className="imageContainer">
-                                    <img src="asset/developer.jpg" alt="" />
+                                    <img src={d.icon} alt="" />
                                 </div>
-                                <h2>Title</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
                                 <span>Projects</span>
                             </div>
                         </div>
-                        <div className="right"></div>
+                        <div className="right">
+                            <img src="asset/image1.jpg" alt="" />
+                        </div>
                     </div>
                 </div>
+                ))}
             </div>
+            <img src="asset/arrow.png" className="arrow left" onClick={()=>handleClick("left")} alt="" />
+            <img src="asset/arrow.png" className="arrow right" onClick={()=>handleClick()} alt="" />
         </div>
     )
 }
